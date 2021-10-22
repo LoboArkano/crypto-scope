a14985bc8b1bf20743ef97e1c2c32b92d446ffe5
LoboArkano <pipiolo_mananes@hotmail.com>
2021-10-22 14:26:28 -0500

    Import App.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { /* Switch, Route, Link */ } from 'react-router-dom';
 import { /* Layout, Typography, Space */ } from 'antd';
 import { Navbar } from './components';
+import './stylesheets/css/App.css';
 
 const App = () => (
   <div className="app">
