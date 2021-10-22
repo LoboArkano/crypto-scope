bc914706b88f7950e743d3252601f9fa1091e820
LoboArkano <pipiolo_mananes@hotmail.com>
2021-10-22 14:44:17 -0500

    Add menu links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
-  /* Button, Menu, */ Typography, Avatar,
+  /* Button, */Menu, Typography, Avatar,
 } from 'antd';
 import { Link } from 'react-router-dom';
 import {
-/* HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined */
+  HomeOutlined, FundOutlined, MoneyCollectOutlined, BulbOutlined, /* MenuOutlined */
 } from '@ant-design/icons';
 import icon from '../stylesheets/images/icon.svg';
 
@@ -17,6 +17,20 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Crypto Scope</Link>
         </Typography.Title>
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />} key="home">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />} key="cryptocurrencies">
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />} key="exchanges">
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />} key="news">
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
         {/* <Button className="menu-control-container">
 
         </Button> */}
