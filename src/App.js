b0c9342f6754fe5cb4a3abda3aae6f1e847ed129
LoboArkano <pipiolo_mananes@hotmail.com>
2021-10-22 14:22:05 -0500

    Update App component Add Index.html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+/* eslint-disable react/self-closing-comp */
 import React from 'react';
+import { /* Switch, Route, Link */ } from 'react-router-dom';
+import { /* Layout, Typography, Space */ } from 'antd';
+import { Navbar } from './components';
 
-const App = () => {
-  return (
-    <div>
-      
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
     </div>
-  )
-}
+    <div className="main"></div>
+    <div className="footer"></div>
+  </div>
+);
 
 export default App;
