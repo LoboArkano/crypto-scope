c583d0ea1dd57478a52620346eea3ebb60fa5c11
LoboArkano <pipiolo_mananes@hotmail.com>
2021-10-22 15:59:31 -0500

    Wrap components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
+export { default as Cryptocurrencies } from './Cryptocurrencies';
+export { default as CryptoDetails } from './CryptoDetails';
+export { default as Exchanges } from './Exchanges';
+export { default as Homepage } from './Homepage';
 export { default as Navbar } from './Navbar';
+export { default as News } from './News';
