e0dded7039f7590b6558005990e929625ab6e83c
LoboArkano <pipiolo_mananes@hotmail.com>
2021-10-26 09:54:40 -0500

    Add cryptocurrencies to the homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,8 +3,9 @@ import millify from 'millify';
 import {
   Typography, Row, Col, Statistic,
 } from 'antd';
-/* import { Link } from 'react-router-dom'; */
+import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '.';
 
 const { Title } = Typography;
 
@@ -34,6 +35,20 @@ const Homepage = () => {
           <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Crypto currencies in the world</Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 };
