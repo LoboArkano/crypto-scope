27b419817c68d095b5ed77b08e104820d11e7004
LoboArkano <pipiolo_mananes@hotmail.com>
2021-11-04 11:18:19 -0600

    Fix link

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -36,7 +36,7 @@ const Cryptocurrencies = ({ simplified }) => {
       <Row gutter={[24, 24]} className="crypto-card-container">
         {crypto?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-            <Link to={`/currency/${currency.id}`}>
+            <Link to={`/crypto/${currency.id}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
                 extra={(
